f875d927a20e849b4b110438186a59e794803952
Monish Kacha <monishkacha@gmail.com>
2026-07-28 01:05:06 +0530

    Fix uploads folder

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -1,15 +1,21 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
-        const uniqueName =
-            Date.now() + "-" + file.originalname;
-
+        const uniqueName = Date.now() + "-" + file.originalname;
         cb(null, uniqueName);
     }
 });
